75299d4eb293e381a0842c4aaaad59f9c51c462c
koradiyayash26 <yashkoradiya26@gmail.com>
2025-06-04 22:49:50 +0530

    https://scrapedl-vercel.vercel.app/ base url added

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, 
-    allowedHosts: ['fb7d-43-250-156-199.ngrok-free.app','8ceb-43-250-156-199.ngrok-free.app','127.0.0.1:8000'],
+    allowedHosts: true,
   },
 })
