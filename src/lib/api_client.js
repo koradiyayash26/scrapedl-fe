42bb38075ca1a365918f31c8b0ea3b640f7b9e2d
koradiyayash26 <yashkoradiya26@gmail.com>
2025-06-07 18:45:21 +0530

    base url chnage

--- src/lib/api_client.js
+++ src/lib/api_client.js
@@ -5,7 +5,7 @@ const isLocalDevelopment = window.location.hostname === "localhost" || window.lo
 const apiClient = axios.create({
   baseURL: isLocalDevelopment 
     ? "http://127.0.0.1:8000"
-    : "https://scrapedl-vercel.vercel.app",
+    : "https://scrapedl.vercel.app/",
 });
 
 export default apiClient;
